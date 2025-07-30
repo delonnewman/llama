83fcd4b8bcc78ac3b7ff40d3521960d359a6beda
Delon Newman <contact@delonnewman.name>
2025-07-30 10:04:12 -0400

    Refactor chomped strength tests

--- lib/Llama/Core.pm
+++ lib/Llama/Core.pm
@@ -2,13 +2,12 @@ package Llama::Core;
 use strict;
 use warnings;
 use utf8;
-use feature 'signatures';
 
 use Exporter 'import';
 our @EXPORT_OK = qw(chomped);
 
-sub chomped :prototype($) ($in) {
-  local $_ = $in;
+sub chomped :prototype($) {
+  local $_ = shift;
   s/\n$//;
   $_;
 }
--- t/Llama/Core.t
+++ t/Llama/Core.t
@@ -5,5 +5,6 @@ use lib qw(../../lib);
 use Llama::Core qw(chomped);
 
 is chomped "hiya\n", "hiya";
+is chomped "hiya", "hiya";
 
 done_testing;
