f4c16d0a8a51bbc49b57a1545cf122e6cc8a6c42
Delon Newman <contact@delonnewman.name>
2025-07-31 14:54:22 -0400

    Add modules for Attribute support

---
+++ lib/Llama/Attribute.pm
@@ -0,0 +1,9 @@
+package Llama::Attribute;
+use strict;
+use warnings;
+use utf8;
+use feature 'signatures';
+
+# Attribute meta object
+
+1;
---
+++ lib/Llama/Attributes.pm
@@ -0,0 +1,9 @@
+package Llama::Attributes;
+use strict;
+use warnings;
+use utf8;
+use feature 'signatures';
+
+# macro functions for declaratively building attributes
+
+1;
