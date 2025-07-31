725600f1c117897ee65ca46fd9d56a6bd3fa738d
Delon R. Newman <contact@delonnewman.name>
2025-07-30 23:25:31 -0400

    Use signatures

--- lib/Llama/IO.pm
+++ lib/Llama/IO.pm
@@ -2,23 +2,19 @@ package Llama::IO;
 use strict;
 use warnings;
 use utf8;
+use feature 'signatures';
 
 use Exporter 'import';
 our @EXPORT_OK = qw(slurp spit);
 
-sub slurp :prototype($) {
-  my $file = shift;
-
+sub slurp :prototype($) ($file) {
   open my $fh, '<', $file or die "can't read $file";
   local $/='' unless wantarray;
 
   <$fh>;
 }
 
-sub spit :prototype($$) {
-  my $file = shift;
-  my $content = shift;
-
+sub spit :prototype($$) ($file, $content) {
   open my $fh, '>', $file or die "can't write to $file";
   print $fh $content;
 
