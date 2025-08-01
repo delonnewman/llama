18ea56b196c502b2e8d711f1ae62ae023958db86
Delon R. Newman <contact@delonnewman.name>
2025-07-31 23:50:36 -0400

    Add Null

---
+++ lib/Llama/Null.pm
@@ -0,0 +1,57 @@
+package Llama::Null;
+use strict;
+use warnings;
+use utf8;
+use feature 'signatures';
+use feature 'state';
+
+use overload 'bool' => sub{0};
+
+use Llama::Object qw(:base :abstract);
+
+sub if_null ($self, $block) {
+  $block->();
+  $self;
+}
+
+{
+  no strict 'refs';
+  no warnings 'once';
+  *if_falsy = \&if_null;
+}
+
+sub if_truthy ($self, $_block) { $self }
+
+sub SCALAR {
+  state $scalar = Llama::Null::Scalar->allocate('');
+}
+
+sub HASH {
+  state $hash = Llama::Null::Hash->allocate;
+}
+
+sub ARRAY {
+  state $array = Llama::Null::Array->allocate;
+}
+
+sub CODE {
+  state $code = Llama::Null::Code->allocate(sub{@_});
+}
+
+package Llama::Null::Scalar {
+  use Llama::Object qw(+ScalarObject +Null);
+}
+
+package Llama::Null::Hash {
+  use Llama::Object qw(+HashObject +Null);
+}
+
+package Llama::Null::Array {
+  use Llama::Object qw(+ArrayObject +Null);
+}
+
+package Llama::Null::Code {
+  use Llama::Object qw(+CodeObject +Null);
+}
+
+1;
