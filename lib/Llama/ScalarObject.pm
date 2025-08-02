3c98797f2b6990c316a5553ca6a9b718ed973042
Delon Newman <contact@delonnewman.name>
2025-08-01 20:40:37 -0400

    Remove instance class logic from ScalarObject

--- lib/Llama/ScalarObject.pm
+++ lib/Llama/ScalarObject.pm
@@ -9,21 +9,10 @@ use Llama::Object qw(:base :constructor);
 use overload bool => sub{1};
 
 sub allocate ($class, $value) {
-  my $object_id = Llama::Object->OBJECT_ID;
-  my $id = sprintf("0x%06X", $object_id);
-  my $instance_class = "$class=OBJECT($id)";
-
-  {
-    no strict 'refs';
-    push @{$instance_class . '::ISA'}, $class;
-    *{$instance_class . '::object_id'} = sub { $object_id }
-  }
-
-  bless \$value, $instance_class;
+  bless \$value, $class;
 }
 
 sub value ($self) { $$self }
 sub to_int ($self) { int($self->value) }
-sub to_string ($self) { ref($self) }
 
 1;
