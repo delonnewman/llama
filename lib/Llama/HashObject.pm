fd0c48fb308cd0ac905faaf8fd2cc8324d76d780
Delon Newman <contact@delonnewman.name>
2025-08-01 22:52:49 -0400

    Add HashRef

--- lib/Llama/HashObject.pm
+++ lib/Llama/HashObject.pm
@@ -4,12 +4,14 @@ use warnings;
 use utf8;
 use feature 'signatures';
 
-use Carp ();
-
 use Llama::Object qw(:base :constructor);
+
+use overload '%{}' => sub{shift->HashRef};
 
 sub allocate ($class, %attributes) {
   bless \%attributes, $class;
 }
+
+sub HashRef ($self) { $self }
 
 1;
