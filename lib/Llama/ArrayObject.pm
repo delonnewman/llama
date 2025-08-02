e098acac25f7d15adc6fe50abd91133f85b0e190
Delon Newman <contact@delonnewman.name>
2025-08-01 22:53:57 -0400

    Add ArrayRef

--- lib/Llama/ArrayObject.pm
+++ lib/Llama/ArrayObject.pm
@@ -6,8 +6,12 @@ use feature 'signatures';
 
 use Llama::Object qw(:base :constructor);
 
+use overload '@{}' => sub{shift->ArrayRef};
+
 sub allocate ($class, @values) {
   bless \@values, $class;
 }
+
+sub ArrayRef ($self) { $self }
 
 1;
