ba54db336a714fa3658ca4ed98321b8d167663a2
Delon Newman <contact@delonnewman.name>
2025-08-04 17:59:16 -0400

    Add beginning to Attribute

--- lib/Llama/Attribute.pm
+++ lib/Llama/Attribute.pm
@@ -6,4 +6,19 @@ use feature 'signatures';
 
 # Attribute meta object
 
+use Carp ();
+
+use Llama::Object qw(+HashObject :constructor);
+
+my @ATTRIBUTES = qw(name type validate);
+
+sub BUILD ($self, $name, %options) {
+  %$self = (name => $name, %options);
+  $self->LOCK(@ATTRIBUTES);
+}
+
+sub validate ($self, $value) {
+
+}
+
 1;
