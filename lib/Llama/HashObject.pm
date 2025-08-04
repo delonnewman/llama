4d5fdaad09987207aa58529470eb032b06397fba
Delon Newman <contact@delonnewman.name>
2025-08-04 17:58:20 -0400

    Add locking to HashObject

--- lib/Llama/HashObject.pm
+++ lib/Llama/HashObject.pm
@@ -4,6 +4,8 @@ use warnings;
 use utf8;
 use feature 'signatures';
 
+use Hash::Util ();
+
 use Llama::Object qw(:base :constructor);
 
 use overload '%{}' => sub{shift->HashRef};
@@ -11,6 +13,15 @@ use overload '%{}' => sub{shift->HashRef};
 sub allocate ($class) {
   bless {}, $class;
 }
+
+sub LOCK ($self, @keys) {
+  my @attributes = (keys %$self, @keys);
+  Hash::Util::lock_keys(%$self, @attributes);
+  Hash::Util::lock_value(%$self, $_) for @attributes;
+  $self;
+}
+
+sub LOCKED ($self) { Hash::Util::hash_locked(%$self) }
 
 sub HashRef ($self) { $self }
 
