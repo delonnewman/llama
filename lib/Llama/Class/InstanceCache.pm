9d14f6b3a953cd34bc90b50f31572783f717dc7d
Delon Newman <contact@delonnewman.name>
2025-08-04 16:00:59 -0400

    Replace META_CLASS with KEY

--- lib/Llama/Class/InstanceCache.pm
+++ lib/Llama/Class/InstanceCache.pm
@@ -5,15 +5,15 @@ use utf8;
 use feature 'signatures';
 no strict 'refs';
 
-use constant META_CLASS => '__META_CLASS__';
+sub KEY :prototype() { '__META_CLASS__' }
 
 sub get ($class_name) {
-  my $sym = $class_name . '::' . META_CLASS;
+  my $sym = $class_name . '::' . KEY;
   ${$sym};
 }
 
 sub set ($class_name, $instance) {
-  my $sym = $class_name . '::' . META_CLASS;
+  my $sym = $class_name . '::' . KEY;
   ${$sym} = $instance;
   $instance;
 }
