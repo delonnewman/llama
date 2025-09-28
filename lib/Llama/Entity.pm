785e3b78ac27eac8ba6fff17863ef3a5bbc326a1
Delon R. Newman <contact@delonnewman.name>
2025-09-27 22:39:44 -0400

    Remove assign_attributes

--- lib/Llama/Entity.pm
+++ lib/Llama/Entity.pm
@@ -1,14 +1,4 @@
 package Llama::Entity;
 use Llama::Base qw(+Base::Hash :signatures);
 
-sub assign_attributes ($self, @args) {
-  return unless @args;
-
-  my %attributes = @args > 1 ? @args : $args[0]->%*;
-  $self->$_($attributes{$_} // die "$_ is required") for $self->class->required_attributes;
-  $self->$_($attributes{$_}) for $self->class->optional_attributes;
-
-  return $self;
-}
-
 1;
