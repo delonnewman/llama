bca61e7458ac486bd6aa05fa9150fa3eebaa6610
Delon R. Newman <contact@delonnewman.name>
2025-09-27 22:39:04 -0400

    Add value_type

--- lib/Llama/Attribute.pm
+++ lib/Llama/Attribute.pm
@@ -22,6 +22,7 @@ delegate [qw(is_mutable is_optional is_valid)] => 'type';
 
 sub is_required ($self) { !$self->is_optional }
 
+sub value_type ($self) { $self->type->value }
 sub type ($self) { $self->{type} }
 sub name ($self) { $self->{name} }
 
