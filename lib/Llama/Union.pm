d391df1bb69bfb51490dee2a28969222d8634191
Delon R. Newman <contact@delonnewman.name>
2025-09-28 22:57:16 -0400

    Add Llama::Union

---
+++ lib/Llama/Union.pm
@@ -0,0 +1,24 @@
+package Llama::Union;
+use Llama::Base qw(:base :signatures);
+
+use Llama::Union::Class;
+use Llama::Union::Member;
+
+no warnings 'experimental::signatures';
+
+sub import($class, @union) {
+  my $name  = caller;
+  my $union = Llama::Union::Class->new($name)->build($class);
+
+  $union->name->add($_) for @union;
+}
+
+sub add($class, $key) {
+  Carp::croak "invalid usage should only be called on subclasses" if $class eq __PACKAGE__;
+
+  Llama::Union::Member->new($class, $key)->build;
+
+  return $class;
+};
+
+1;
---
+++ lib/Llama/Union/Member.pm
@@ -0,0 +1,62 @@
+package Llama::Union::Member;
+
+=encoding utf8
+
+=head1 NAME
+
+Llama::Union::Member
+
+=head2 DESCRIPTION
+
+A meta class for building enum members (see L<Llama::Union> and L<Llama::Union::Class>)
+
+=cut
+
+use Llama::Base qw(:base :signatures);
+
+use Scalar::Util qw(blessed);
+
+no strict 'refs';
+
+# Class Methods
+
+sub new ($class, $union_class, $key) {
+  bless [$union_class, uc $key], $class;
+}
+
+# Attributes
+
+sub union_class ($self) { $self->[0] }
+sub key ($self) { $self->[1] }
+sub name ($self) { join '::' => @$self }
+
+# Instance Methods
+
+sub build ($self) {
+  # Make member class a subclass of the union class
+  $self->subclass($self->union_class);
+
+  # Create member instance
+  my $name     = $self->name;
+  my $instance = bless \$name, $name;
+
+  # Add enum class method for accessing this instance e.g MyEnum->KEY
+  $self->add_accessor_method($instance);
+
+  # Update index
+  ${$self->union_class . '::MEMBERS'}{$self->key} = $instance;
+
+  return $self;
+}
+
+sub subclass ($self, $superclass) {
+  @{$self->name . '::ISA'} = $superclass;
+  return $self;
+}
+
+sub add_accessor_method ($self, $instance) {
+  *{$self->name} = sub :prototype() { $instance };
+  return $self;
+}
+
+1;
