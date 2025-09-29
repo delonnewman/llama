1ca3c3ece88f4e737410795db01c3c277173490a
Delon R. Newman <contact@delonnewman.name>
2025-09-29 15:35:51 -0400

    Add Exception and StackFrame

---
+++ lib/Llama/Exception.pm
@@ -0,0 +1,53 @@
+package Llama::Exception;
+
+use utf8;
+use strict;
+use warnings;
+use feature qw(:5.20 signatures);
+no warnings 'experimental::signatures';
+
+use overload '""' => sub{shift->Str}, 'bool' => sub{1};
+
+use Llama::StackFrame;
+
+sub new ($class, $message) {
+  my $name  = ref($class) || $class;
+  my $trace = Llama::StackFrame->trace;
+  bless { name => $name, message => $message, stacktrace => $trace }, $class;
+}
+
+sub name       ($self) { $self->{name} }
+sub message    ($self) { $self->{message} }
+sub stacktrace ($self) { $self->{stacktrace} }
+
+{
+  # aliases
+  no strict 'refs';
+  *__name__ = \&name;  
+}
+
+sub Str ($self) {
+  my $name    = $self->name;
+  my $message = $self->message;
+  my $trace   = join "\n" => map { "  $_" } $self->stacktrace->@*;
+
+  return "$name: $message\n$trace";
+}
+
+package Llama::TypeError {
+  our @ISA = qw(Llama::Exception);
+}
+
+package Llama::ArgumentError {
+  our @ISA = qw(Llama::Exception);
+}
+
+package Llama::NotImplementedError {
+  our @ISA = qw(Llama::Exception);
+}
+
+package Llama::AttributeError {
+  our @ISA = qw(Llama::Exception);
+}
+
+1;
