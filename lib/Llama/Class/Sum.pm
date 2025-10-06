05bc4d1c26868389c848855693c34f6c3e99185c
Delon R. Newman <contact@delonnewman.name>
2025-10-05 20:16:06 -0400

    Add Class::Sum::new

--- lib/Llama/Class/Sum.pm
+++ lib/Llama/Class/Sum.pm
@@ -1,4 +1,14 @@
 package Llama::Class::Sum;
-use Llama::Base qw(+Class :signatures)
+use Llama::Base qw(+Class :signatures);
+
+sub new ($class, $name, @members) {
+  my $sum = $class->next::method($name);
+
+  for my $member (@members) {
+    $member->append_superclasses($name);
+  }
+
+  return $sum;
+}
 
 1;
