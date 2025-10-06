0297e6f084ebab3f63509dd1bdd4df9fd0d1912a
Delon R. Newman <contact@delonnewman.name>
2025-10-06 14:57:34 -0400

    Add invalidate

--- lib/Llama/Class/InstanceCache.pm
+++ lib/Llama/Class/InstanceCache.pm
@@ -10,13 +10,18 @@ sub KEY :prototype() { '__META_CLASS__' }
 
 sub get ($, $class_name) {
   my $sym = $class_name . '::' . KEY;
-  ${$sym};
+  return ${$sym};
 }
 
 sub set ($, $class_name, $instance) {
   my $sym = $class_name . '::' . KEY;
   ${$sym} = $instance;
-  $instance;
+  return $instance;
+}
+
+sub invalidate ($class, $class_name) {
+  $class->set($class_name, undef);
+  return $class;
 }
 
 1;
