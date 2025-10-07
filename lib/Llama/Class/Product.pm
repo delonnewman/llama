85a5bad4f808525eb385eb397aa740b01cb03d05
Delon R. Newman <contact@delonnewman.name>
2025-10-07 14:39:10 -0400

    Fix synax error

--- lib/Llama/Class/Product.pm
+++ lib/Llama/Class/Product.pm
@@ -1,7 +1,8 @@
 package Llama::Class::Product;
-use Llama::Base qw(+Class :signatures)
+use Llama::Base qw(+Class :signatures);
 
-sub add_member ($self, $member, $name = $member->name) {
+sub add_member ($self, $member, $name = undef) {
+  $name //= $member->name;
   $self->add_attribute($name, $member);
   return $self;
 }
