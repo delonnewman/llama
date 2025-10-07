81638c738d82546497f9babea46c1de401d57549
Delon R. Newman <contact@delonnewman.name>
2025-10-07 15:05:17 -0400

    Make $options optional

--- lib/Llama/Attributes.pm
+++ lib/Llama/Attributes.pm
@@ -5,12 +5,14 @@ use Llama::Package;
 
 # macro functions for declaratively building attributes
 
+my $EMPTY_HASH = {};
+
 sub import ($class) {
   my $caller = caller;
   my $pkg    = Llama::Package->named($caller);
 
   my $order = 0;
-  $pkg->add_sub('has', sub ($name, $options) {
+  $pkg->add_sub('has', sub ($name, $options = $EMPTY_HASH) {
     $caller->class->add_attribute($name, { order => $order++, %$options });
   });
 }
