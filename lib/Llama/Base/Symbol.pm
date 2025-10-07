b7cf2bdff80ab3dfd124037c3b4fe7907365f8a4
Delon R. Newman <contact@delonnewman.name>
2025-10-07 13:20:42 -0400

    Rename %cache to %SYMBOLS

--- lib/Llama/Base/Symbol.pm
+++ lib/Llama/Base/Symbol.pm
@@ -1,10 +1,10 @@
 package Llama::Base::Symbol;
 use Llama::Base qw(+Base :signatures);
 
-my %cache = ();
+my %SYMBOLS = ();
 
 sub new ($class) {
-  $cache{$class} //= bless \$class, $class;
+  return $SYMBOLS{$class} //= bless \$class, $class;
 }
     
 1;
