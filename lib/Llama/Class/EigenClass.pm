25576486840f94d1092b2d840912c1cfccbd42a4
Delon Newman <contact@delonnewman.name>
2025-10-08 16:27:48 -0400

    Add progenitor to EigenClass

--- lib/Llama/Class/EigenClass.pm
+++ lib/Llama/Class/EigenClass.pm
@@ -4,15 +4,27 @@ use Llama::Prelude qw(+Class :signatures);
 sub build($class, $mirror) {
   my $new_class  = $class->new;
   my $orig_class = $mirror->class;
-  my $name = $orig_class->name;
+  my $name       = $orig_class->name;
 
   # make original class a super class
-  $new_class->append_superclasses($orig_class->name);
+  $new_class->progenitor($orig_class->name);
 
   # bless object into new class
   $mirror->BLESS($new_class->name);
 
+
   return $new_class;
+}
+
+sub progenitor ($self, @args) {
+  no strict 'refs';
+  if (@args) {
+    $self->superclasses($args[0]);
+    ${$self->package->qualify('ATTRIBUTE_DATA')}{__progenitor__} = $args[0];
+    return $self;
+  }
+
+  return ${$self->package->qualify('ATTRIBUTE_DATA')}{__progenitor__};
 }
 
 1;
