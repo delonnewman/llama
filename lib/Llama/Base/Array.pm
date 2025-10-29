baba9427d1cbd715a03da3a9dbad310746536efa
Delon Newman <contact@delonnewman.name>
2025-10-29 13:38:54 -0400

    Clean up Base::Array

--- lib/Llama/Base/Array.pm
+++ lib/Llama/Base/Array.pm
@@ -1,12 +1,8 @@
 package Llama::Base::Array;
 use Llama::Prelude qw(+Base :signatures);
 
-use overload '@{}' => sub{shift->toArrayRef};
-
 sub allocate ($class, @args) {
-  bless [], $class;
+  bless \@args, $class;
 }
-
-sub toArrayRef ($self) { $self }
 
 1;
