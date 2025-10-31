4b93d54f28aa7ce7de954ee9f64b6a5928f9e5d9
Delon Newman <contact@delonnewman.name>
2025-10-31 18:42:01 -0400

    Add Llama::Callable

---
+++ lib/Llama/Callable.pm
@@ -0,0 +1,20 @@
+package Llama::Callable;
+use Llama::Prelude qw(:signatures);
+
+use overload '&{}' => 'toCodeRef';
+
+use Llama::Exception;
+
+sub call ($self, @args) {
+  die Llama::NotImplementedError->new('must be implemented by subclasses');
+}
+
+sub toCodeRef ($self) {
+  return sub { $self->call(@_) };
+}
+
+sub partial ($self, @outer) {
+  return sub (@inner) { $self->call(@outer, @inner) }
+}
+
+1;
