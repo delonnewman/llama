9e8beef4254784147c85d66780de1e995d24806e
Delon Newman <contact@delonnewman.name>
2025-11-07 15:05:13 -0500

    Tidy Parser::Result

--- lib/Llama/Parser/Result.pm
+++ lib/Llama/Parser/Result.pm
@@ -8,21 +8,26 @@ sub new ($class, %attributes) {
   bless \%attributes => $class;
 }
 
-package Llama::Parser::Result::Ok;
-our @ISA = qw(Llama::Parser::Result);
-
-sub value ($self) { $self->{value} }
-sub rest ($self) { $self->{rest} }
-sub is_terminal ($self) { !$self->rest }
 sub is_error { 0 }
-sub is_ok { 1 }
-
-package Llama::Parser::Result::Error;
-our @ISA = qw(Llama::Parser::Result);
-
-sub message ($self) { $self->{message} }
-sub is_terminal { 1 }
-sub is_error { 1 }
 sub is_ok { 0 }
+
+package Llama::Parser::Result::Ok {
+  our @ISA = qw(Llama::Parser::Result);
+
+  sub value ($self) { $self->{value} }
+  sub rest ($self) { $self->{rest} }
+
+  sub is_terminal ($self) { !$self->rest }
+  sub is_ok { 1 }
+}
+
+package Llama::Parser::Result::Error {
+  our @ISA = qw(Llama::Parser::Result);
+
+  sub message ($self) { $self->{message} }
+
+  sub is_terminal { 1 }
+  sub is_error { 1 }
+}
 
 1;
