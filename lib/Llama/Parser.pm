213b220623a87456719270177c506c3dc3f9ca63
Delon R. Newman <contact@delonnewman.name>
2025-12-28 22:51:46 -0500

    Remove unused methods

--- lib/Llama/Parser.pm
+++ lib/Llama/Parser.pm
@@ -13,7 +13,7 @@ use Llama::Parser::Result;
 sub Result :prototype() { 'Llama::Parser::Result' }
 
 use Exporter 'import';
-our @EXPORT_OK = qw(choice any_of collect HashObject);
+our @EXPORT_OK = qw(choice any_of collect);
 
 #
 # Exported Combinators
@@ -63,134 +63,6 @@ sub collect (@parsers) {
 
 sub new ($class, $sub) {
   return bless $sub => $class;
-}
-
-sub coerce ($class, $parser) {
-  return $parser if blessed($parser) && $parser->isa(__PACKAGE__);
-  return $class->Literal($parser) unless blessed($parser);
-
-  Carp::croak "can't coerce " . np($parser) . " into a parser";
-}
-
-sub Undef ($class) {
-  state $Undef = $class->__name__->new(sub ($input) {
-    return Result->Ok(value => $input) unless defined $input;
-
-    Result->Error(message => "is not undefined got " . np($input));
-  });
-}
-
-sub Defined ($class) {
-  state $Defined = $class->__name__->new(sub ($input) {
-    return Result->Ok(value => $input) if defined $input;
-
-    Result->Error(message => "is not defined");
-  });
-}
-
-sub Any ($class) {
-  state $Any = $class->__name__->new(sub ($input) {
-    return Result->Ok(value => $input);
-  });
-}
-
-sub Bool ($class) {
-  state $Bool = $class->__name__->new(sub ($input) {
-    return Result->Ok(value => !!0) if !$input;
-    return Result->Ok(value => !!1) if $input eq '1';
-
-    Result->Error(message => np($input) . " is not a valid boolean value");
-  });
-}
-
-sub Str ($class) {
-  state $Str = $class->__name__->new(sub ($input) {
-    return Result->Ok(value => "$input") if defined($input) && ref(\$input) eq 'SCALAR';
-
-    Result->Error(message => np($input) . " is not a valid string value");
-  });
-}
-
-sub Num ($class) {
-  state $Num = $class->__name__->new(sub ($input) {
-    return Result->Ok(value => 0+$input) if defined($input) && looks_like_number($input);
-
-    Result->Error(message => "is not a valid number got ". np($input));
-  });
-}
-
-sub Literal ($class, $val)  {
-  $class->__name__->new(sub ($input) {
-    return Result->Ok(value => $input) if defined($input) && $input eq $val;
-
-    Result->Error(message => "expected literal " . np($val) . " got " . np($input));
-  });
-}
-
-sub Array ($class, $parser = $class->Any) {
-  $class->__name__->new(sub ($input) {
-    return Result->Error(message => "only array references are valid instead got " . np($input))
-      if ref($input) ne 'ARRAY';
-
-    my @results = map { $parser->run($_) } @$input;
-    my @errors  = grep { $_->is_error } @results;
-
-    if (@errors) {
-      my $i = 0;
-      return Result->CompositeError(messages => [map { "index " . $i++ . " " . $_->message } @errors]);
-    }
-
-    return Result->Ok(value => [map { $_->value } @results]);
-  });
-}
-
-sub HasKey ($class, $key, $value = $class->Defined) {
-  my $parser = $class->MayHaveKey($key, $value);
-  $class->__name__->new(sub ($input) {
-    my $result = $parser->run($input);
-    return Result->Error(message => "key " . np($key) . " is missing")
-      if $result->is_ok && !defined $result->value;
-
-    return $result;
-  });
-}
-
-sub MayHaveKey ($class, $key, $value = $class->Defined) {
-  $class->__name__->new(sub ($input) {
-    return Result->Error(message => "only hash references are valid instead got " . np($input))
-      if ref($input) ne 'HASH';
-
-    return Result->Ok(rest => $input) unless exists $input->{$key};
-
-    my $result = $value->run($input->{$key});
-    return Result->Error(message => "key " . np($key) . " " . $result->message) if $result->is_error;
-
-    my @keys = grep { $_ ne $key } keys %$input;
-    my %rest = %{$input}{@keys};
-    my $pair = [$key, $result->value];
-
-    return Result->Ok(value => $pair, rest => %rest ? \%rest : undef);
-  });
-}
-
-sub Keys ($class, %schema) {
-  collect(map { $class->HasKey($_ => $schema{$_}) } keys %schema);
-}
-*RequiredKeys = \&Keys;
-
-sub OptionalKeys ($class, %schema) {
-  collect(map { $class->MayHaveKey($_ => $schema{$_}) } keys %schema);
-}
-
-sub HashObject ($class_name, @parsers) {
-  my $parser = collect(@parsers);
-  __PACKAGE__->new(sub ($input) {
-    my $result = $parser->run($input);
-    return $result if $result->is_error;
-
-    my $obj = bless toHashRef($result->value) => $class_name;
-    return Result->Ok(value => $obj);
-  });
 }
 
 #
