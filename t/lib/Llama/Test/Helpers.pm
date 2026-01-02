dc3e35d7619fc5c4ab15d51b64456a3895b0147c
Delon R. Newman <contact@delonnewman.name>
2026-01-02 18:02:20 -0500

    Add error_ok and result_ok

--- t/lib/Llama/Test/Helpers.pm
+++ t/lib/Llama/Test/Helpers.pm
@@ -3,13 +3,29 @@ use Llama::Prelude qw(:signatures);
 use Llama::Test::TestSuite;
 
 use Exporter 'import';
-our @EXPORT_OK = qw(parse_ok parse_error_ok);
+our @EXPORT_OK = qw(
+  parse_ok
+  parse_error_ok
+  error_ok
+  result_ok
+);
+
+sub result_ok ($result, %options) {
+  my $name = $options{-name} // '';
+
+  isa_ok $result => "Llama::Parser::Result" => $name;
+  isa_ok $result => "Llama::Parser::Result::Ok" => $name;
+
+  my $msg = 'result is ok';
+  $msg .= ": $name" if $name;
+
+  ok $result->is_ok => $msg;
+}
 
 sub parse_ok ($parser, $val, @args) {
   my $result = $parser->run($val);
 
-  isa_ok $result => "Llama::Parser::Result" => np($val);
-  isa_ok $result => "Llama::Parser::Result::Ok" => np($val);
+  result_ok $result, -name => np($val);
 
   if (@args > 0) {
     if (ref $args[0]) {
@@ -20,15 +36,25 @@ sub parse_ok ($parser, $val, @args) {
     }
   }
 
-  is $result->rest  => $args[1] if @args > 1;
+  is $result->rest => $args[1] if @args > 1;
+}
+
+sub error_ok ($result, $pattern = undef, %options) {
+  my $name = $options{-name} // '';
+
+  isa_ok $result => "Llama::Parser::Result" => $name;
+  isa_ok $result => "Llama::Parser::Result::Error" => $name;
+
+  my $msg = 'result is error';
+  $msg .= ": $name" if $name;
+
+  ok $result->is_error => $msg;
+  like $result->message => $pattern if $pattern;
 }
 
 sub parse_error_ok ($parser, $val, $pattern = undef) {
   my $result = $parser->run($val);
-
-  isa_ok $result => "Llama::Parser::Result" => np($val);
-  isa_ok $result => "Llama::Parser::Result::Error" => np($val);
-  like $result->message => $pattern if $pattern;
+  error_ok $result, $pattern, -name => np($val);
 }
 
 1;
