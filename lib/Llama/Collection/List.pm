b4c7bb85acabd905cff5f2cbbfbe5ab1a6e386fd
Delon R. Newman <contact@delonnewman.name>
2026-01-02 19:20:37 -0500

    Add Collection::List and Colleciton::Sequence

--- lib/Llama/Collection/List.pm
+++ lib/Llama/Collection/List.pm
@@ -1,4 +1,25 @@
 package Llama::Collection::List;
-use Llama::Prelude qw(+Base::Array +Sequence);
+use Llama::Prelude qw(+Base::Array +Collection::Sequence :signatures);
+
+sub first  ($self) { $self->[0] }
+sub next   ($self) { $self->[1] }
+sub length ($self) { $self->[2] }
+
+sub cons ($self, $value) {
+  return $self->__name__->new($value, undef, 1) if $self->is_empty;
+
+  $self->__name__->new($value, $self, $self->length + 1);
+}
+
+sub empty ($self) {
+  state $empty = $self->__name__->new(undef, undef, 0);
+}
+
+sub toStr ($self) {
+  my $name    = $self->__name__;
+  my $entries = join ', ' => map { "$_" } $self->toArrayRef->@*;
+
+  return "$name($entries)";
+}
 
 1;
--- lib/Llama/Collection/Sequence.pm
+++ lib/Llama/Collection/Sequence.pm
@@ -1,4 +1,29 @@
 package Llama::Collection::Sequence;
-use Llama::Prelude qw(+Collection :role);
+use Llama::Prelude qw(+Collection :signatures);
+
+sub first { die "NotImplementedError: first" }
+sub next  { die "NotImplementedError: next" }
+sub empty { die "NotImplementedError: empty" }
+
+sub rest ($self) {
+  return $self->next // $self->empty;
+}
+
+sub is_empty ($self) {
+  return $self->length == 0;
+}
+
+sub toArrayRef ($self) {
+  my @array;
+  return \@array if $self->is_empty;
+
+  my $seq = $self;
+  while ($seq) {
+    push @array => $seq->first;
+    $seq = $seq->next;
+  }
+
+  return \@array;
+}
 
 1;
