fdd56269882f6d3048fe043b7cc8d6ac70d55158
Delon Newman <contact@delonnewman.name>
2026-09-11 21:20:20 -0400

    Rename $ns to $pkg

--- lib/Llama/Method.pm
+++ lib/Llama/Method.pm
@@ -3,8 +3,8 @@ use Llama::Prelude qw(+Base::Code :signatures);
 
 sub new ($class, $object, $method, @args) {
   my $self = $class->next::method(sub { $object->$method(@args, @_) });
-  my $ns   = ref($object) || $object;
-  $self->set_name("$ns\::$method");
+  my $pkg  = ref($object) || $object;
+  $self->set_name("$pkg\::$method");
 }
 
 1;
