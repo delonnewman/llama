072bdb90695478676656cdd78635e2522f9620c9
Delon Newman <contact@delonnewman.name>
2026-09-11 21:19:56 -0400

    Fix method naming

--- lib/Llama/Method.pm
+++ lib/Llama/Method.pm
@@ -3,7 +3,8 @@ use Llama::Prelude qw(+Base::Code :signatures);
 
 sub new ($class, $object, $method, @args) {
   my $self = $class->next::method(sub { $object->$method(@args, @_) });
-  $self->set_name(ref($object) . "::$method");
+  my $ns   = ref($object) || $object;
+  $self->set_name("$ns\::$method");
 }
 
 1;
